7e71e59a66643a2e60b44ae2bf4440fec60f89c4
Gil Barbara <gilbarbara@gmail.com>
2020-03-02 03:01:43 -0300

    Update configs

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'test/tsconfig.json',
+    },
+  },
+  moduleDirectories: ['node_modules', 'src', './'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  preset: 'ts-jest',
+  setupFiles: ['<rootDir>/test/__setup__/setupFiles.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/__setup__/setupTests.ts'],
+  testMatch: null,
+  testRegex: '/test/.*?\\.(test|spec)\\.tsx?$',
+  testURL: 'http://localhost:1337/',
+  verbose: false,
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+};
