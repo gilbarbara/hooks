546053d60968938fab0dd997193080f3a832d28a
Gil Barbara <gilbarbara@gmail.com>
2020-12-20 15:21:04 -0300

    Add useSingleton hook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as useFetch, FetchStatus } from './use-fetch';
 export { default as useScript } from './use-script';
+export { default as useSingleton } from './use-singleton';
 export { default as useThrottle } from './use-throttle';
 export { default as useThrottleValue } from './use-throttle-value';
 export { default as useWhyDidYouUpdate } from './use-why-did-you-update';
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,17 @@
-import { useFetch, useScript, useThrottle, useThrottleValue, useWhyDidYouUpdate } from '../src';
+import {
+  useFetch,
+  useScript,
+  useSingleton,
+  useThrottle,
+  useThrottleValue,
+  useWhyDidYouUpdate,
+} from '../src';
 
 describe('hooks', () => {
   it('should have all exports', () => {
     expect(useFetch).toEqual(expect.any(Function));
     expect(useScript).toEqual(expect.any(Function));
+    expect(useSingleton).toEqual(expect.any(Function));
     expect(useThrottle).toEqual(expect.any(Function));
     expect(useThrottleValue).toEqual(expect.any(Function));
     expect(useWhyDidYouUpdate).toEqual(expect.any(Function));
