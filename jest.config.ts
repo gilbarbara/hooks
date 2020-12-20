6e515c4badf89fe54cce8d61bc0d06c665f9e277
Gil Barbara <gilbarbara@gmail.com>
2020-12-20 15:23:58 -0300

    Upgrade dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -19,6 +19,7 @@ module.exports = {
   preset: 'ts-jest',
   setupFiles: ['<rootDir>/test/__setup__/setupFiles.ts'],
   setupFilesAfterEnv: ['<rootDir>/test/__setup__/setupTests.ts'],
+  snapshotSerializers: ['jest-serializer-html'],
   testMatch: null,
   testRegex: '/test/.*?\\.(test|spec)\\.tsx?$',
   testURL: 'http://localhost:1337/',
