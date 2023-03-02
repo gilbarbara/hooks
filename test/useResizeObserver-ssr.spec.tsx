1bfc02e4093c4e979525a685296cf8d10d330872
Gil Barbara <gilbarbara@gmail.com>
2023-03-01 21:28:06 -0300

    Add useResizeObserver hook

---
+++ test/useResizeObserver-ssr.spec.tsx
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment node
+ */
+
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
+
+import { useResizeObserver } from '../src/useResizeObserver';
+
+function Component() {
+  useResizeObserver('#root');
+
+  return <div />;
+}
+
+describe('useResizeObserver-ssr', () => {
+  it('should work without a DOM', () => {
+    const view = renderToString(<Component />);
+
+    expect(view).toMatchSnapshot();
+  });
+});
