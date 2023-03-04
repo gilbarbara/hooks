750ba6c09c5f648f5713e908a959a6481fb3a62a
Gil Barbara <gilbarbara@gmail.com>
2023-03-04 02:22:23 -0300

    Replace useResize with useWindowSize

---
+++ src/useWindowSize.ts
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { canUseDOM } from './utils';
+
+export interface WindowSize {
+  height: number;
+  width: number;
+}
+
+export function useWindowSize(debounce = 0) {
+  const [size, setSize] = useState<WindowSize>({
+    height: 0,
+    width: 0,
+  });
+  const timeoutRef = useRef<number>(0);
+
+  const handleResize = useRef(() => {
+    window.clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = window.setTimeout(() => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }, debounce);
+  });
+
+  useEffect(() => {
+    /* istanbul ignore next */
+    if (!canUseDOM) {
+      return () => undefined;
+    }
+
+    const getSize = handleResize.current;
+
+    setSize({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+    window.addEventListener('resize', getSize);
+
+    return () => window.removeEventListener('resize', getSize);
+  }, []);
+
+  return size;
+}
