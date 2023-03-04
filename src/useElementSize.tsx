ae0b33c9e0eab03a937a9e14efd63eb0aa76f45b
Gil Barbara <gilbarbara@gmail.com>
2023-03-04 19:28:02 -0300

    Update useElementSize response

--- src/useElementSize.tsx
+++ src/useElementSize.tsx
@@ -5,22 +5,18 @@ import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 import { useResizeObserver } from './useResizeObserver';
 import { getElement } from './utils';
 
-export type ElementSize = Omit<DOMRectReadOnly, 'toJSON'> & {
-  blockSize: number;
-  inlineSize: number;
-};
+export interface ElementSize {
+  height: number;
+  innerHeight: number;
+  innerWidth: number;
+  width: number;
+}
 
 const defaultState: ElementSize = {
-  blockSize: 0,
-  bottom: 0,
   height: 0,
-  inlineSize: 0,
-  left: 0,
-  right: 0,
-  top: 0,
+  innerHeight: 0,
+  innerWidth: 0,
   width: 0,
-  x: 0,
-  y: 0,
 };
 
 export function useElementSize<T extends Element>(target: Target<T>, debounce = 0): ElementSize {
@@ -44,10 +40,10 @@ export function useElementSize<T extends Element>(target: Target<T>, debounce = 
       return;
     }
 
-    const { bottom, height, left, right, top, width, x, y } = entry.contentRect;
+    const { height, width } = entry.contentRect;
     const { blockSize, inlineSize } = entry.borderBoxSize[0];
 
-    setDimensions({ blockSize, bottom, height, inlineSize, left, right, top, width, x, y });
+    setDimensions({ height: blockSize, innerHeight: height, innerWidth: width, width: inlineSize });
   }, [entry]);
 
   return dimensions;
