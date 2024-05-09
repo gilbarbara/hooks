a80c3d00aaa5eafbb70f9d47601dc97e2e1470fd
Gil Barbara <gilbarbara@gmail.com>
2024-05-09 02:52:50 -0300

    Add useDebounce

---
+++ demo/src/examples/UseDebounce.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { Box, Button, Input, Paragraph } from '@gilbarbara/components';
+import { useDebounce } from '@gilbarbara/hooks';
+
+import Block from '../components/Block';
+import Heading from '../components/Heading';
+
+export default function UseDebounce() {
+  const [state, setState] = useState('No typing yet...');
+  const [value, setValue] = useState('');
+  const [debouncedValue, setDebouncedValue] = useState('');
+
+  const { cancel } = useDebounce(
+    () => {
+      setState('Typing stopped');
+      setDebouncedValue(value);
+    },
+    1000,
+    [value],
+  );
+
+  return (
+    <Block>
+      <Heading>useDebounce</Heading>
+      <Box maxWidth={400} mx="auto">
+        <Input
+          name="debounced-input"
+          onChange={({ currentTarget }) => {
+            setState('Waiting for typing to stop...');
+            setValue(currentTarget.value);
+          }}
+          placeholder="Debounced input"
+          type="text"
+          value={value}
+        />
+        <Paragraph italic mt="xs">
+          {state}
+        </Paragraph>
+        <Paragraph bold my="md">
+          Debounced value: {debouncedValue}
+        </Paragraph>
+        <Button
+          onClick={() => {
+            cancel();
+            setState('Debounce canceled');
+          }}
+          size="sm"
+        >
+          Cancel debounce
+        </Button>
+      </Box>
+    </Block>
+  );
+}
