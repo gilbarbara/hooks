d4a4f2427d7ac5409ed2b1077f217a4f1a271900
Gil Barbara <gilbarbara@gmail.com>
2025-01-27 21:03:38 -0300

    Upgrade useFetch
    - replace vitest-fetch-mock with msw

--- test/__setup__/vitest.setup.ts
+++ test/__setup__/vitest.setup.ts
@@ -1,11 +1,5 @@
 import '@testing-library/jest-dom';
 
 import * as matchers from 'jest-extended';
-import { vi } from 'vitest';
-import createFetchMock from 'vitest-fetch-mock';
-
-const fetchMock = createFetchMock(vi);
-
-fetchMock.enableMocks();
 
 expect.extend(matchers);
